20d29bd67626a52080ee7d79a7ead338a6ac72b4
Njeri Marion <njeriwmarion@gmail.com>
2023-07-10 15:57:36 +0300

    object relationships

--- Order/models.py
+++ Order/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from customer.models import Customer
+from cart.models import Cart
 
 # Create your models here.
 class Order(models.Model):
     order_number = models.CharField(max_length=32, unique=True)
-    customer_name = models.OneToOneField(Customer, null=True, on_delete =models.CASCADE)
+    customer_name = models.ForeignKey(Cart, null=True, on_delete =models.CASCADE)
+    customer_name = models.ForeignKey(Customer, null=True, on_delete =models.CASCADE)
     customer_email = models.EmailField(max_length=255)
     customer_phone_number = models.CharField(max_length=15)
     order_date = models.DateTimeField(auto_now_add=True)
